d8b8458a7aa58b999a2ff3ea4e88b2a3210738c3
Arthur <osartie@gmail.com>
2023-01-22 16:01:48 +0300

    первый классовый компонент

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <hr />
+      <ClassCounter />
     </div>
   );
 }
